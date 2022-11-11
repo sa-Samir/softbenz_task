a9efcb4b34ccc65da83879130c653a17c22f54d4
Samir Khadka <samirkhadka89@gmail.com>
2022-11-11 09:35:14 +0545

    description ongoing

--- lib/views/product/description.dart
+++ lib/views/product/description.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -49,6 +50,7 @@ class ProductDescription extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var imageIndex = 0.obs;
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -91,93 +93,182 @@ class ProductDescription extends StatelessWidget {
                       ],
                     ),
                   ),
-                  child: Container(
-                    height: 250,
-                    width: 250,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        color: Constants.pColor,
-                      ),
-                      color: Colors.white,
-                    ),
-                    child: images.isEmpty
-                        ? const Center(
-                            child: Text('No Image'),
-                          )
-                        : CarouselSlider(
-                            items: images
-                                .map((e) => Builder(
-                                      builder: (BuildContext context) {
-                                        return Stack(
-                                          children: [
-                                            Container(
-                                              height: 250,
-                                              width: 250,
-                                              padding: const EdgeInsets.all(5),
-                                              child: Image.network(
-                                                e,
-                                                fit: BoxFit.contain,
-                                              ),
-                                            ),
-                                            Positioned(
-                                              top: 0,
-                                              right: 15,
-                                              child: Container(
-                                                decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(8),
-                                                  color: Colors.grey.shade200,
-                                                ),
-                                                child: IconButton(
-                                                  tooltip: 'Share image',
-                                                  onPressed: () {},
-                                                  padding:
-                                                      const EdgeInsets.all(2),
-                                                  constraints:
-                                                      const BoxConstraints(),
-                                                  icon: const Icon(
-                                                    Icons.share,
-                                                    size: 18,
-                                                  ),
-                                                  color: Constants.pColor,
-                                                ),
-                                              ),
-                                            ),
-                                          ],
-                                        );
-                                      },
-                                    ))
-                                .toList(),
-                            options: CarouselOptions(
-                              viewportFraction: 1,
-                              autoPlay: true,
-                              enableInfiniteScroll: false,
-                            ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 250,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                            color: Constants.pColor,
                           ),
+                          color: Colors.white,
+                        ),
+                        child: images.isEmpty
+                            ? const Center(
+                                child: Text('No Image'),
+                              )
+                            : CarouselSlider(
+                                items: images
+                                    .map((e) => Builder(
+                                          builder: (BuildContext context) {
+                                            return Stack(
+                                              children: [
+                                                Container(
+                                                  height: 250,
+                                                  width: 250,
+                                                  padding:
+                                                      const EdgeInsets.all(5),
+                                                  child: Image.network(
+                                                    e,
+                                                    fit: BoxFit.contain,
+                                                  ),
+                                                ),
+                                                Positioned(
+                                                  top: 0,
+                                                  right: 15,
+                                                  child: Container(
+                                                    decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              8),
+                                                      color:
+                                                          Colors.grey.shade200,
+                                                    ),
+                                                    child: IconButton(
+                                                      tooltip: 'Share image',
+                                                      onPressed: () {},
+                                                      padding:
+                                                          const EdgeInsets.all(
+                                                              2),
+                                                      constraints:
+                                                          const BoxConstraints(),
+                                                      icon: const Icon(
+                                                        Icons.share,
+                                                        size: 18,
+                                                      ),
+                                                      color: Constants.pColor,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        ))
+                                    .toList(),
+                                options: CarouselOptions(
+                                    viewportFraction: 1,
+                                    autoPlay: true,
+                                    enableInfiniteScroll: false,
+                                    onPageChanged: (index, reason) {
+                                      imageIndex.value = index;
+                                    }),
+                              ),
+                      ),
+                      images.isEmpty
+                          ? const SizedBox()
+                          : Obx(
+                              () => DotsIndicator(
+                                dotsCount: images.length,
+                                position: imageIndex.value.toDouble(),
+                                decorator: DotsDecorator(
+                                  size: const Size.square(9.0),
+                                  activeSize: const Size(18.0, 9.0),
+                                  activeShape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5.0)),
+                                ),
+                              ),
+                            ),
+                    ],
                   ),
                 ),
                 Positioned(
                   bottom: 15,
                   right: 15,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: Colors.grey.shade200,
-                    ),
-                    padding: const EdgeInsets.all(8),
-                    child: FavoriteButton(
-                      isFavorite: false,
-                      valueChanged: (value) {
-                        debugPrint('Added to facourites');
-                      },
-                      iconSize: 32,
-                      iconColor: Constants.pColor,
-                      iconDisabledColor: Colors.black38,
+                  child: Row(
+                    children: List.generate(
+                      5,
+                      (index) => Icon(
+                        index < rating ? Icons.star : Icons.star_border,
+                        color: Constants.pColor,
+                        size: 22,
+                      ),
                     ),
                   ),
                 ),
               ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: Constants.screenPadding),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        flex: 2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(name),
+                            Text(seller),
+                          ],
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.grey.shade300,
+                              ),
+                              child: IconButton(
+                                tooltip: 'Message $seller',
+                                padding: const EdgeInsets.all(4),
+                                constraints: const BoxConstraints(),
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.mail_outline,
+                                  color: Constants.pColor,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(4),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.grey.shade300,
+                              ),
+                              child: FavoriteButton(
+                                isFavorite: false,
+                                valueChanged: (value) {
+                                  debugPrint('Added to facourites');
+                                },
+                                iconSize: 32,
+                                iconColor: Constants.pColor,
+                                iconDisabledColor: Colors.black38,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
